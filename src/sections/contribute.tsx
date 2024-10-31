9b59793a7f7807cab21c65f13ee5dd0bedc8dfa4
reggidev <reggidev@gmail.com>
2024-10-31 18:33:16 -0300

    feat: ensure max width is correct

--- src/sections/contribute.tsx
+++ src/sections/contribute.tsx
@@ -2,7 +2,7 @@ export function Contribute() {
   return (
     <section className="px-10 py-10 lg:px-32">
       <div className="mx-auto flex h-[384px] max-w-7xl flex-col items-center justify-center rounded-[20px] bg-contribute-background bg-cover bg-no-repeat">
-        <span className="max-w-[830px] px-10 text-center text-2xl font-bold text-white md:text-5xl">
+        <span className="max-w-[900px] px-10 text-center text-2xl font-bold text-white md:text-5xl">
           You can contribute to provide a place for children with special needs!
         </span>
 
@@ -13,9 +13,11 @@ export function Contribute() {
             </div>
           </a>
 
-          <div className="rounded-lg border bg-white px-8 py-4 text-center font-medium transition-all duration-200 hover:border hover:border-secondary hover:bg-primary">
-            Donate
-          </div>
+          <a href="#">
+            <div className="rounded-lg border bg-white px-8 py-4 text-center font-medium transition-all duration-200 hover:border hover:border-secondary hover:bg-primary">
+              Donate
+            </div>
+          </a>
         </div>
       </div>
     </section>
