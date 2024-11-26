c52c86e8989c1302d3dd0212fa8d9eed98faaa05
reggidev <reggidev@gmail.com>
2024-11-26 14:15:08 -0300

    feat: add scroll link to projects section

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,16 +1,30 @@
+import { Link as ScrollLink } from 'react-scroll'
+
 export function Hero() {
   return (
-    <section className="h-[750px] w-full bg-hero-background bg-cover bg-bottom bg-no-repeat">
-      <div className="mx-auto flex h-full max-w-[1175px] flex-col items-center justify-center px-10 know-about-us:items-start">
+    <section className="relative h-[750px] w-full bg-hero-background bg-cover bg-bottom bg-no-repeat">
+      {/* Escurecer imagem */}
+      <div className="absolute inset-0 bg-black/50" />
+
+      {/* Conteúdo do Hero */}
+      <div className="relative z-10 mx-auto flex h-full max-w-[1175px] flex-col items-center justify-center px-10 know-about-us:items-start">
         <h1 className="max-w-[600px] text-center text-5xl font-bold text-white know-about-us:text-left">
           Cuidando do planeta para as próximas gerações
         </h1>
 
-        <a href="#">
-          <div className="mt-12 rounded-lg border bg-white px-8 py-4 transition-all duration-200 hover:border hover:border-secondary hover:bg-primary">
+        <ScrollLink
+          to="projects"
+          href="projects"
+          spy={true}
+          smooth={true}
+          offset={-88}
+          duration={500}
+          className="mt-8"
+        >
+          <div className="rounded-lg border bg-white px-8 py-4 transition-all duration-200 hover:border hover:border-secondary hover:bg-primary">
             Nossas ações
           </div>
-        </a>
+        </ScrollLink>
       </div>
     </section>
   )
